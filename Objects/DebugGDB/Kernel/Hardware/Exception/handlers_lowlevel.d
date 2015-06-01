87f757cedde0e902beda2f5cf88ad5fa691bc953
Vladimir Sorokin <v.sorokin@confident.spb.ru>
2015-06-01 18:16:24 +0300

    remove deps files

--- Objects/DebugGDB/Kernel/Hardware/Exception/handlers_lowlevel.d
+++
@@ -1,4 +0,0 @@
-./Hardware/Exception/handlers_lowlevel.o:  \
- /root/Chaos/Sources/StormG4/Hardware/Exception/handlers_lowlevel.S \
-  /root/Chaos/Enviroments/System/ia32/Exceptions/codes.h \
-  /root/Chaos/Enviroments/Kernel/Common/exception.h
--- Objects/Release/Kernel/Hardware/Irq/handlers_lowlevel.d
+++
@@ -1,2 +0,0 @@
-./Hardware/Irq/handlers_lowlevel.o:  \
- /root/Chaos/Sources/StormG4/Hardware/Irq/handlers_lowlevel.S
